1f404da83fbe935f8e728123ebd6659a4905961f
Melvin-King <79367726+Melvin-King@users.noreply.github.com>
2023-04-17 06:52:45 +0800

    ..

--- src/main/java/com/mybatisplus_comp3334/entity/TransactionRecord.java
+++ src/main/java/com/mybatisplus_comp3334/entity/TransactionRecord.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,6 +22,6 @@ public class TransactionRecord {
 
     private Long buyerId;
 
-    private java.sql.Timestamp transactionTime;
+    private Timestamp transactionTime;
 
 }
