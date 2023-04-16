bfc0c71aac4fedcbb551ddb12e7caa15a9df07a4
Melvin-King <79367726+Melvin-King@users.noreply.github.com>
2023-04-17 02:50:43 +0800

    Update TransactionRecord.java

--- src/main/java/com/mybatisplus_comp3334/entity/TransactionRecord.java
+++ src/main/java/com/mybatisplus_comp3334/entity/TransactionRecord.java
@@ -1,4 +1,23 @@
 package com.mybatisplus_comp3334.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public interface TransactionRecord {
+
+    private Long estateId;
+
+    private Long sellerId;
+
+    private Long buyerId;
+
+    private String transactionTime;
+
 }
