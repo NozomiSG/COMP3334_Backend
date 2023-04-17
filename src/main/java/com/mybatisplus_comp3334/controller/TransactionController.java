dcff48d72ef4379955119907a7bb793b31630fed
Melvin-King <79367726+Melvin-King@users.noreply.github.com>
2023-04-17 15:06:27 +0800

    Update TransactionController.java

--- src/main/java/com/mybatisplus_comp3334/controller/TransactionController.java
+++ src/main/java/com/mybatisplus_comp3334/controller/TransactionController.java
@@ -2,8 +2,10 @@ package com.mybatisplus_comp3334.controller;
 
 import com.mybatisplus_comp3334.entity.Estate;
 import com.mybatisplus_comp3334.entity.TransactionRecord;
+import com.mybatisplus_comp3334.entity.User;
 import com.mybatisplus_comp3334.service.concept.EstateService;
 import com.mybatisplus_comp3334.service.concept.TransactionService;
+import com.mybatisplus_comp3334.util.EncryptionUtils;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -23,11 +25,21 @@ public class TransactionController {
     @Autowired
     private EstateService estateService;
 
+    @Autowired
+    EncryptionUtils encryptionUtils;
 
-    @PostMapping("/insert-transaction")
-    public Map<String, Object> insertTransaction(@ModelAttribute TransactionRecord transaction) {
+    @PostMapping("/insert-transaction") //to be edited after completion of front end: input as encrypted/signatured String
+    public Map<String, Object> insertTransaction(@RequestParam Long buyerId, @RequestParam Long sellerId, @RequestParam String transString) {
         log.info("insert transaction request");
         Map<String, Object> map = new HashMap<>(4);
+        TransactionRecord checkUser = transactionService.
+        if (checkUser == null) {
+            log.info("email haven't registered");
+            map.put("resultCode", "0");
+            map.put("resultMsg", "verify reject: email haven't registered");
+            map.put("data", "reject");
+            return map;
+        }
         if (transaction == null) {
             log.info("insert transaction reject: invalid request");
             map.put("resultCode", "0");
